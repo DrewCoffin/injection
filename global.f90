8b3634a8317301cd5c0caea5c578fcb3dfdedfa7
DACAK197 <dacoffin@alaska.edu>
2019-03-20 10:54:25 -0800

    Establish

---
+++ global.f90
@@ -0,0 +1,24 @@
+!equivalent to cm3_model_common in Steffl
+!holds dybnamic global variables
+!constant values are in varTypes.f90 with structure declarations
+
+MODULE GLOBAL
+
+USE varTypes
+
+real                 :: rates,trans,net_source,trans_exp,iondens0,net_source0,tau0,longitude
+integer              :: info,runt,dt
+real                 :: zoff,rdist, v_r0
+real                 :: sys3variations,lag_amp,lag_phase,lag_const,fehot_amp,fehot_phase,fehot_const, lon3
+real                 :: Fex, Fs, Fsp, Fs2p, Fs3p, Fo, Fop, Fo2p !used in fluxCorrect in timestep module
+real                 :: EFex, EFsp, EFs2p, EFs3p, EFop, EFo2p, EFelec !Used in updateNT in timestep module
+
+type(recomb)         ::rec_tables !Holds recombinaion tables read in dielectronics.f90
+
+logical              ::trans_type=.false.
+
+real                 ::protons=0.0  !<can be added to inputs.dat
+
+integer              ::procno=3    !processor number for print statements, 0 is inner boundary
+
+END MODULE GLOBAL
---
+++ plots/organize.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+cd ./data/
+
+rm -r sp/
+rm -r s2p/
+rm -r s3p/
+rm -r op/
+rm -r o2p/
+rm -r elec/
+rm -r ex/
+rm -r LOAD/
+rm -r MOUT/
+rm -r VSUB/
+#rm -r OXGN/
+#rm -r SLFR/
+
+#mkdir OXGN
+#mkdir SLFR
+mkdir LOAD
+mkdir MOUT
+mkdir VSUB
+
+good=$?
+if [ $good -ne 0 ]
+  then
+  echo "LOAD folder already exists. Please remove or change name to procede." 
+fi
+
+mv LOAD*.dat LOAD/.
+mv MOUT*.dat MOUT/.
+mv VSUB*.dat VSUB/.
+#mv OXGN*.dat OXGN/.
+#mv SLFR*.dat SLFR/.
+
+cd ../
+./organizeSpecies.sh sp
+./organizeSpecies.sh s2p
+./organizeSpecies.sh s3p
+./organizeSpecies.sh op
+./organizeSpecies.sh o2p
+./organizeSpecies.sh elec
+./organizeSpecies.sh ex  
+
